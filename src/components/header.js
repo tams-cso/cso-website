5f1995b94fad6ad610ffa31414db7c6dc17caa40
Ben Taylor <bentaylor@bentaylor.me>
2019-06-13 01:45:37 -0500

    fix: fixes overflow on small width devices

--- src/components/header.js
+++ src/components/header.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 import { Box, Flex, Heading, Text } from 'rebass'
 import CircleLogo from './circle-logo'
 import Navigation from './navigation'
+import './header.css'
 
 class Header extends Component {
    render = () => {
       return (
          <Flex alignItems='center' bg='black' px={40} style={{height: '100vh'}}>
-            <Box width={2/3}>
+            <Box width={2/3} className='halfWhenSmall'>
                <CircleLogo />
             </Box>
-            <Box width={1/3}>
+            <Box width={1/3} className='halfWhenSmall'>
                <Navigation links={[
                      {
                         href: '/',
