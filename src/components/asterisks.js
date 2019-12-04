98ab2505bf1cff2b96a8f3b7be5a5af59a5f8caf
Ben Taylor <bentaylor@bentaylor.me>
2019-12-04 10:52:14 -0600

    fix responsive asterisks

--- src/components/asterisks.js
+++ src/components/asterisks.js
@@ -3,17 +3,17 @@ import React from 'react';
 const Asterisks = () => (
    <div style={{
       position: 'absolute',
-      left: 200,
+      right: 900,
       transform: 'scale(2)'
    }} className='asterisks'>
       <p style={{
-         fontSize: 'calc(40px + 10vmin)',
+         fontSize: '100px',
          position: 'absolute',
          color: 'yellow',
          textShadow: '-3px -3px 0 #fff, -6px -6px 0 #000, 3px 3px 0 white, 4px 4px 0 magenta'
       }}>*</p>
       <p style={{
-         fontSize: 'calc(30px + 8vmin)',
+         fontSize: '80px',
          position: 'absolute',
          color: 'magenta',
          left: -20,
@@ -21,7 +21,7 @@ const Asterisks = () => (
          textShadow: '-3px -3px 0 #fff, -6px -6px 0 #000, 3px 3px 0 white, 4px 4px 0 cyan'
       }}>*</p>
       <p style={{
-         fontSize: 'calc(80px + 16vmin)',
+         fontSize: '200px',
          position: 'absolute',
          color: 'cyan',
          top: -220,
